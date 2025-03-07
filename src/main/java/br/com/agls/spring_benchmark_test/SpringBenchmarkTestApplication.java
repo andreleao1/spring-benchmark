a773c6ec86a8012678cb550f4060cabd88c778d7
andreleao <andre.gustavo.ls@hotmail.com>
2025-03-06 21:13:21 -0300

    first commir

---
+++ src/main/java/br/com/agls/spring_benchmark_test/SpringBenchmarkTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.agls.spring_benchmark_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBenchmarkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBenchmarkTestApplication.class, args);
+	}
+
+}
